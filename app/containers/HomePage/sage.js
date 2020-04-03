8bbedcd0c5bf21ccff464867dc3037c9ae779b5c
S1MB10T3 <s1mb10t3@pm.me>
2020-04-03 18:51:44 -0400

    added scss and blank layouts for future components

---
+++ app/containers/HomePage/sage.js
@@ -0,0 +1,5 @@
+// import { call, put, select, takeLatest } from 'redux-saga/effects';
+// import { LOAD_REPOS } from 'containers/App/constants';
+// import { reposLoaded, repoLoadingError } from 'containers/App/actions';
+
+// import request from 'utils/request';
--- app/utils/corona.js
+++
@@ -1,9 +0,0 @@
-import axios;
-
-// API GET gets NYC cases
-
-// API GET NY State cases
-
-// API GET US cases
-
-// API Get Global Cases
